0d58e4ec663a3959d5df9d3f07f7515016c741ef
dairyisfine <willb1220@gmail.com>
2024-06-16 23:17:22 -0500

    i'm a genious - #very smart

--- lib/collision.cpp
+++ lib/collision.cpp
@@ -1,6 +1,7 @@
 
 #include <vector>
 #include <_xlocale.h>
+#include <_xlocale.h>
 
 #include "objects.h"
 #include "raylib.h"
@@ -26,45 +27,42 @@ collision_response check_all_collision(Rectangle check_rectangle, vector<Rectang
     vector<Rectangle> checkable_solids = solids;
     int checkable_solids_count = solid_count;
     int collision_line_length = scene.get_collision_line_length();
+    vector<float> check_middle = vector<float>{check_rectangle.x + check_rectangle.width/2, check_rectangle.y + check_rectangle.height/2};
 
-    float collision_point_index = pixel_location_to_collision_index(normalize(check_rectangle.x/8)*8, normalize(check_rectangle.y/8)*8);
-    float left_collision_point_index = pixel_location_to_collision_index(check_rectangle.x, normalize(check_rectangle.y/8)*8);
-    float x_right_point = check_rectangle.x+check_rectangle.width;
-    float right_collision_point_index = pixel_location_to_collision_index(x_right_point-1, check_rectangle.y);
-
-    Rectangle tile_collider_rectangle = check_rectangle;
-
-    int collision_axis_rounding_correction = 0;
+    int checkable_tile_middle_index;
+    vector<int> checkable_tile_indexes = vector<int>();
 
     if (x_axis) {
         if (direction > 0) {
-            if (checkable_tiles_collision[collision_point_index+1]) {
-                tile_collider_rectangle.x = collision_index_to_pixel_location(collision_point_index+1)[0];
-                checkable_solids.push_back(tile_collider_rectangle);
-                checkable_solids_count++;
-            }
-        } else if (direction < 0) {
-            if (checkable_tiles_collision[collision_point_index-1]) {
-                tile_collider_rectangle.x = collision_index_to_pixel_location(collision_point_index-1)[0];
-                checkable_solids.push_back(tile_collider_rectangle);
-                checkable_solids_count++;
-            }
+            checkable_tile_middle_index = pixel_location_to_collision_index(check_middle[0]+check_rectangle.width, check_middle[1]);
+        } else {
+            checkable_tile_middle_index = pixel_location_to_collision_index(check_middle[0]-check_rectangle.width, check_middle[1]);
         }
+        checkable_tile_indexes.push_back(checkable_tile_middle_index);
+        checkable_tile_indexes.push_back(checkable_tile_middle_index+collision_line_length);
+        checkable_tile_indexes.push_back(checkable_tile_middle_index-collision_line_length);
     } else {
         if (direction > 0) {
-            if (checkable_tiles_collision[left_collision_point_index + collision_line_length] || checkable_tiles_collision[right_collision_point_index + collision_line_length]) {
-                tile_collider_rectangle.y = collision_index_to_pixel_location(collision_point_index+scene.get_collision_line_length())[1];
-                checkable_solids.push_back(tile_collider_rectangle);
-                checkable_solids_count++;
-            }
-        } else if (direction < 0) {
-            if (checkable_tiles_collision[collision_point_index - collision_line_length]) {
-                tile_collider_rectangle.y = collision_index_to_pixel_location(collision_point_index-scene.get_collision_line_length())[1];
-                checkable_solids.push_back(tile_collider_rectangle);
-                checkable_solids_count++;
-            }
+            checkable_tile_middle_index = pixel_location_to_collision_index(check_middle[0], check_middle[1]+check_rectangle.height);
+        } else {
+            checkable_tile_middle_index = pixel_location_to_collision_index(check_middle[0], check_middle[1]-check_rectangle.height);
+        }
+        checkable_tile_indexes.push_back(checkable_tile_middle_index);
+        checkable_tile_indexes.push_back(checkable_tile_middle_index+1);
+        checkable_tile_indexes.push_back(checkable_tile_middle_index-1);
+    }
+
+    for (int i = 0; i < checkable_tile_indexes.size(); i++) {
+        if (checkable_tiles_collision[checkable_tile_indexes[i]]) {
+            vector<int> pixel_location = collision_index_to_pixel_location(checkable_tile_indexes[i]);
+            Rectangle checkable_tile = {(float)pixel_location[0], (float)pixel_location[1], 8, 8};
+            checkable_solids.push_back(checkable_tile);
+            checkable_solids_count++;
         }
     }
+
+
+
 
     for (int i = 0; i < checkable_solids_count; i++) {
         DrawRectangleRec(checkable_solids[i], RED);
